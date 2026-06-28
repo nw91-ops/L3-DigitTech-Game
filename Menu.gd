0ca7f7b118cfe7a886a51419b42d8356c611f3a3
nw91-ops <nw91@mahurangi.school.nz>
2026-06-29 08:54:50 +1200

    Commit 8
    Added more functionality to the battle scenes, now able to select attack and play the animation and do damage and the enemy will retaliate.

--- Menu.gd
+++ Menu.gd
@@ -7,4 +7,52 @@ func _ready() -> void:
 	_options_menu.connect_to_buttons()
 	_options_menu.button_focus(0)
 
+extends Node
 
+class_name Character
+
+@export var character_name : String
+@export var max_hp := 100
+@export var max_mp := 25
+@export var attack := 15
+@export var defense := 5
+@export var speed := 10
+
+var hp : int
+var mp : int
+var defending := false
+
+func _ready():
+	hp = max_hp
+	mp = max_mp
+
+func take_damage(amount:int):
+
+	var damage = max(amount - defense, 1)
+
+	if defending:
+		damage /= 2
+
+	hp -= damage
+	hp = max(hp,0)
+
+	print(character_name + " took " + str(damage) + " damage!")
+
+	defending = false
+
+func heal(amount:int):
+
+	hp += amount
+	hp = min(hp,max_hp)
+
+	print(character_name + " healed " + str(amount))
+
+func defend():
+
+	defending = true
+
+	print(character_name + " is defending!")
+
+func is_dead():
+
+	return hp <= 0
--- window_default.gd
+++ window_default.gd
@@ -1,2 +1,101 @@
 class_name Window_Default extends NinePatchRect
 
+extends Node
+
+@onready var player = $Player
+@onready var enemy = $Enemy
+
+enum{
+	ATTACK,
+	HEAL,
+	DEFEND
+}
+
+var state
+
+func _ready():
+
+	start_battle()
+
+func start_battle():
+
+	state = BattleState.BattleState.PLAYER_TURN
+
+	player_turn()
+
+func player_turn():
+
+	print("------------------")
+	print("PLAYER TURN")
+	print("------------------")
+
+	# Eventually replace this with buttons
+	player_choice(ATTACK)
+
+func enemy_turn():
+
+	print("------------------")
+	print("ENEMY TURN")
+	print("------------------")
+
+	var move = randi_range(0,2)
+
+	match move:
+
+		ATTACK:
+			print("Enemy attacks!")
+			player.take_damage(enemy.attack)
+
+		HEAL:
+			enemy.heal(15)
+
+		DEFEND:
+			enemy.defend()
+
+	check_end()
+
+	state = BattleState.BattleState.PLAYER_TURN
+
+	player_turn()
+
+func player_choice(choice):
+
+	match choice:
+
+		ATTACK:
+
+			print("Player attacks!")
+
+			enemy.take_damage(player.attack)
+
+		HEAL:
+
+			player.heal(20)
+
+		DEFEND:
+
+			player.defend()
+
+	check_end()
+
+	state = BattleState.BattleState.ENEMY_TURN
+
+	enemy_turn()
+
+func check_end():
+
+	if enemy.is_dead():
+
+		state = BattleState.BattleState.VICTORY
+
+		print("Victory!")
+
+		get_tree().paused = true
+
+	if player.is_dead():
+
+		state = BattleState.BattleState.DEFEAT
+
+		print("Defeat!")
+
+		get_tree().paused = true
