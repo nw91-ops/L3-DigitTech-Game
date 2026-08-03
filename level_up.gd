0148793c11d9f233831c9c64269e243a92f8610f
nw91-ops <nw91@mahurangi.school.nz>
2026-08-04 10:41:05 +1200

    Commit 14
    Fleshed out the XP system some more and added more stat upgrades.

--- level_up.gd
+++ level_up.gd
@@ -23,3 +23,36 @@ func level_up():
 
 	health_changed.emit(hp, base_max_hp)
 	mana_changed.emit(mp, base_max_mp)
+	
+	func level_up():
+
+	level += 1
+
+	# Award 5 stat points
+	stat_points += 5
+
+	# Restore HP and MP
+	hp = base_max_hp
+	mp = base_max_mp
+
+	experience_needed = int(experience_needed * 1.25)
+
+	check_for_new_skills()
+
+	print(character_name + " reached Level " + str(level) + "!")
+	print("You have " + str(stat_points) + " stat points to spend.")
+
+	health_changed.emit(hp, base_max_hp)
+	mana_changed.emit(mp, base_max_mp)
+	
+	func increase_hp():
+
+	if stat_points <= 0:
+		return
+
+	base_max_hp += 10
+	stat_points -= 1
+
+	hp = min(hp + 10, base_max_hp)
+
+	health_changed.emit(hp, base_max_hp)
